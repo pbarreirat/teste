3cb27e61c133544f12498ea4f145f42ac477649f
Pedro Barreira <pedro.teixeira@mblabs.com.br>
2019-07-01 15:46:46 -0300

    última versão

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Header from './header'
 
 export default class index extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
+ 
   render() {
     return (
       <View>
-        <Text> Legal </Text>
+        <Header />
       </View>
     );
   }
